3f408927ffc2ab8b5b2dd628e0c2f52e4a2458f2
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-10-01 12:25:17 +0200

    WCCS-7 Added Acces-Control Headers

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use((request, response, next) => {
+    response.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods": "DELETE, GET, POST, PUT"
+    });
+    next();
+});
 app.use("/", index);
 app.use("/content-classes", contentClasses);
 app.use("/reference-classes", referenceClasses);
