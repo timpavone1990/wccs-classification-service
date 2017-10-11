f22bc4baa128f1f6d7aa6780f43747d549355b6a
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-10-11 18:40:15 +0200

    WCCS-52 Moved functionality from root endpoint to /classifications

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const morgan = require("morgan");
 const logger = require("./lib/Logger");
-const index = require("./routes/index");
+const classifications = require("./routes/classifications");
 const contentClasses = require("./routes/content-classes");
 const referenceClasses = require("./routes/reference-classes");
 const app = express();
@@ -23,7 +23,7 @@ app.use((request, response, next) => {
     });
     next();
 });
-app.use("/", index);
+app.use("/classifications", classifications);
 app.use("/content-classes", contentClasses);
 app.use("/reference-classes", referenceClasses);
 app.use((error, request, response, next) => {
